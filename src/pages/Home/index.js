03dbf0cad8b9035fa5fa50b9624b85a89768f36b
MHPontes <mateushspontes@gmail.com>
2024-07-04 17:15:24 -0300

    Resolvendo secret API e demais configuracoes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"; // Importing react hooks
 import api from "../../services/api"; // Importing api
 
+
 function Home() {
   const [filmes, setFilmes] = useState([]); // Estado para armazenar os filmes
 
@@ -8,21 +9,32 @@ function Home() {
     async function loadFilmes() {
       const response = await api.get("movie/now_playing", {   //Espera a requisição ser feita com o método await
         params: {
-          api_key: "d1aab98fc9d57be645fb38aec872ebf2",
+          api_key: process.env.APP_API_KEY,
           language: "pt-BR",
           page: 1,
         },
       }) // Requisição para a API
-    
-      console.log(response.data.results); // Exibindo os resultados no console
+      console.log("API Key:", process.env.APP_API_KEY); // Adicione esta linh
+      // console.log(response.data.results); // Exibindo os resultados no console
+
+      setFilmes(response.data.results.slice(0,10)); // Setando os filmes no estado
     }
+
 
     loadFilmes(); // Chamada da função
   }, []); // Array vazio para executar apenas uma vez
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              </article>
+          )
+        })}
+      </div>
     </div>
   );
 }
