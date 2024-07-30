4232647a7bc36d2aa0148458644d1c2af4edfe1f
MHPontes <mateushspontes@gmail.com>
2024-07-30 09:27:24 -0300

    Configurando loading e adicionando tela de erro 404

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,42 +3,56 @@ import api from "../../services/api"; // Importing api
 import { Link } from "react-router-dom"; // Importing Link
 import "./home.css"; // Importing css
 
-
 function Home() {
   const [filmes, setFilmes] = useState([]); // Estado para armazenar os filmes
+  const [loading, setLoading] = useState(true); // Estado para armazenar o loading
 
   useEffect(() => {
     async function loadFilmes() {
-      const response = await api.get("movie/now_playing", {   //Espera a requisição ser feita com o método await
+      const response = await api.get("movie/now_playing", {
+        //Espera a requisição ser feita com o método await
         params: {
-          api_key: "SUA_KEY",
+          api_key: "SUA API",
           language: "pt-BR",
           page: 1,
         },
-      }) // Requisição para a API
-     
+      }); // Requisição para a API
+
       // console.log(response.data.results); // Exibindo os resultados no console
 
-      setFilmes(response.data.results.slice(0,10)); // Setando os filmes no estado de 0 a 10 itens
+      setFilmes(response.data.results.slice(0, 10)); // Setando os filmes no estado de 0 a 10 itens
+      setLoading(false); // Setando o loading como false
     }
-
 
     loadFilmes(); // Chamada da função
   }, []); // Array vazio para executar apenas uma vez
 
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <div className="lista-filmes">
-        {filmes.map((filme) => {        // Mapeando os filmes para exibir na tela
+        {filmes.map((filme) => {
+          // Mapeando os filmes para exibir na tela
           return (
-            <article key={filme.id}>     {/* Key para identificar cada filme */}
-              <strong>{filme.title}</strong>    
+            <article key={filme.id}>
+              {" "}
+              {/* Key para identificar cada filme */}
+              <strong>{filme.title}</strong>
               <img
                 src={`https://image.tmdb.org/t/p/original${filme.poster_path}`} // Imagem do filme
-                alt={filme.title} /> 
-              <Link to={`/filme/${filme.id}`}>Acessar</Link> {/* Link para acessar a página do filme} */}
-              </article>
-          )
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>{" "}
+              {/* Link para acessar a página do filme} */}
+            </article>
+          );
         })}
       </div>
     </div>
