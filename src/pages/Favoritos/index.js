c0a99435b21debed43fbfd5690b9be8c9d4e6651
MHPontes <mateushspontes@gmail.com>
2024-08-05 09:16:31 -0300

    Manipulando lista de filmes, e criando function excluirFilme da lista de favoritos.

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,21 +10,40 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || []); // Setando os filmes no estado ou um array vazio caso não tenha nada no localStorage convertendo de string para array
   }, []);
 
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => { // Filtrando os filmes para excluir o filme com o id passado
+      return item.id !== id;
+    })
+
+    setFilmes(filtroFilmes); // Setando os filmes no estado
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes)); // Salvando a lista de filmes no
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
 
-      <ul> {/* Lista de filmes */} 
-        {filmes.map((item) => {  // Mapeando os filmes para exibir na tela
+        {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
+        
+      <ul>
+        {" "}
+        {/* Lista de filmes */}
+        {filmes.map((item) => {
+          // Mapeando os filmes para exibir na tela
           return (
-            <li key={item.id}>  {/* Key para identificar cada filme, */}
+            <li key={item.id}>
+              {" "}
+              {/* Key para identificar cada filme, */}
               <span>{item.title}</span> {/* Exibindo o título do filme */}
-
               <div>
-              <Link to={`/filme/${item.id}`}>Ver detalhes</Link> {/* Link para acessar a página do filme */}
-                <button>Excluir</button> {/* Botão para excluir o filme */}
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>{" "}
+                {/* Link para acessar a página do filme */}
+                <button onClick={() => excluirFilme(item.id)}>
+                  Excluir
+                </button>{" "}
+                {/* Botão para excluir o filme */}
               </div>
-            </li> 
+            </li>
           );
         })}
       </ul>
