ce3e2ff944547781343cadc95ec6100f07496d06
MHPontes <mateushspontes@gmail.com>
2024-08-05 09:06:55 -0300

    Criando pagina Favoritos

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import "./favoritos.css";
+import { Link } from "react-router-dom";
+
+function Favoritos() {
+  const [filmes, setFilmes] = useState([]); // Estado para armazenar os filmes
+
+  useEffect(() => {
+    const minhaLista = localStorage.getItem("@primeflix"); // Pegando a lista de filmes do localStorage
+    setFilmes(JSON.parse(minhaLista) || []); // Setando os filmes no estado ou um array vazio caso não tenha nada no localStorage convertendo de string para array
+  }, []);
+
+  return (
+    <div className="meus-filmes">
+      <h1>Meus filmes</h1>
+
+      <ul> {/* Lista de filmes */} 
+        {filmes.map((item) => {  // Mapeando os filmes para exibir na tela
+          return (
+            <li key={item.id}>  {/* Key para identificar cada filme, */}
+              <span>{item.title}</span> {/* Exibindo o título do filme */}
+
+              <div>
+              <Link to={`/filme/${item.id}`}>Ver detalhes</Link> {/* Link para acessar a página do filme */}
+                <button>Excluir</button> {/* Botão para excluir o filme */}
+              </div>
+            </li> 
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Favoritos;
